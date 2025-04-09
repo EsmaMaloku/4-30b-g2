2b5d7e36cbcb465f55f3f80a4a202a7e890ae0b7
EsmaMaloku <esmaamalokuu2@gmail.com>
2025-04-09 17:46:25 +0200

    Update main.js

--- 4-30b-g2/js/main.js
+++ 4-30b-g2/js/main.js
@@ -1,1 +1,10 @@
 console.log("Hello!!!");
+function openMenu() {
+	var menu = document.getElementById('myLinks');
+
+	if (menu.style.display === "block") {
+		menu.style.display = "none";
+	}else{
+		menu.style.display = "block";
+	}
+}
