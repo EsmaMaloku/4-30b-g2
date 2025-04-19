5678fc37d9ae157a0ce0af46b609bb3f560e5764
EsmaMaloku <esmaamalokuu2@gmail.com>
2025-04-19 13:47:35 +0200

    Update main.js

--- 4-30b-g2/js/main.js
+++ 4-30b-g2/js/main.js
@@ -1,4 +1,5 @@
 console.log("Hello!!!");
+
 function openMenu() {
 	var menu = document.getElementById('myLinks');
 
@@ -8,3 +9,79 @@ function openMenu() {
 		menu.style.display = "block";
 	}
 }
+
+var slideIndexMS = 1;
+var slideIndexLMS = 1;
+var slideIndexMenu = 1;
+
+var slideIntervalMS, slideIntervalLMS, slideIntervalMenu;
+
+function showSlideMS(n) {
+	var slides = document.getElementsByClassName("ms-slide");
+
+	for (var i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+
+	if (n > slides.length){
+		slideIndexMS = 1;
+	}
+	if (n < 1){
+		slideIndexMS = slides.length;
+	}
+
+	slides[slideIndexMS-1].style.display = "block";
+}
+
+function changeSlideMS(n) {
+	 slideIndexMS += n;
+    showSlideMS(slideIndexMS);
+}
+
+function showSlideLMS(n) {
+	var slides = document.getElementsByClassName("lms-slide");
+
+	for (var i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+
+	if (n > slides.length){
+		slideIndexLMS = 1;
+	}
+	if (n < 1){
+		slideIndexLMS = slides.length;
+	}
+
+	slides[slideIndexLMS-1].style.display = "block";
+}
+
+function changeSlideLMS(n) {
+	 slideIndexLMS += n;
+    showSlideLMS(slideIndexLMS);
+}
+
+function showSlideMenu(n) {
+	var slides = document.getElementsByClassName("menu-slide");
+
+	for (var i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+
+	if (n > slides.length){
+		slideIndexMenu = 1;
+	}
+	if (n < 1){
+		slideIndexMenu = slides.length;
+	}
+
+	slides[slideIndexMenu-1].style.display = "block";
+}
+
+function changeSlideMenu(n) {
+	 slideIndexMenu += n;
+    showSlideMenu(slideIndexMenu);
+}
+
+showSlideMS(slideIndexMS);
+showSlideLMS(slideIndexLMS);
+showSlideMenu(slideIndexMenu);
